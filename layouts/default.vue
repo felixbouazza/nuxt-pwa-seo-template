7dbf41afa5c87faf92262b0994d4fc2a715f7a02
Félix <digitalmine.france@gmail.com>
2022-07-25 23:53:19 +0200

    remove test cache

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,10 +11,6 @@
         </template>
         
         <Nuxt class="bg-blue-500 border minHeightContentMobile md:w-content"/>
-        <ul class="w-full bg-red-100">
-          <button class="w-20 h-10 bg-green-100" @click="testLocalStorage">test</button>
-          <li class="h-10" v-for="el in items" :key="el">el</li>
-        </ul>
     </div>
 
 </template>
@@ -31,11 +27,6 @@ export default {
     changeDisplayPreferenceValue() {
       this.displayPreference = !this.displayPreference
     },
-    async testLocalStorage() {
-      await this.$localForage.setItem("cache", ["icicicicic", "allalalala", "fepkfzep", "feonfze"])
-      this.items = await this.$localForage.getItem("cache")
-      console.log(this.items)
-    }
   }
 };
 </script>
