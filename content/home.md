edb40dc246788f4c3258e8c7dab435d0f1cf0ba4
Félix <digitalmine.france@gmail.com>
2022-08-10 01:11:31 +0200

    add index page

---
+++ content/home.md
@@ -0,0 +1,24 @@
+---
+title: Home
+---
+
+CosmosDocs est un site proposant de la documentation, de la vulgarisation et de l'actualité sur l'écosystème Cosmos (ATOM). Le site est en ligne depuis le mois d'Août 2022 et compte à ce jour 6 collaborateurs / développeurs / rédacteurs.
+
+Nous essayons sur ce site de proposer du contenu simple, accessible et surtout gratuit ! Dans l'objectif d'étendre le périmètre de l'écosystème Cosmos à la communauté francophone.
+
+Pour chaque blockchain, vous retrouverez:
+
+- Un canal __Actualité__: Relayant les infos et news les plus croustillantes du moment.
+- Un canal __Gouvernance__: Sur lequel nous essayons de traduire et vulgariser les votes de gouvernances des cryptomonnaies.
+- Un canal __Airdrop__: Qui ne sera pas forcèment présent sur toutes les cryptomonnaies, car les airdrops sont généralement donnés à la mise en place du Token. Mais qui pourra contenir les airdrops pour lequel posséder et/ou staker le token peut amener à une éligibilité.
+- Un canal __Staking__: Comprenant les news et données relatives au staking (Validateurs, Rendement, APY, etc).
+
+Voici ce que vous devez savoir avant de démarrer :
+
+1. Vous pouvez <a href="https://www.01net.com/astuces/comment-installer-une-version-allegee-de-vos-programmes-preferes-sur-iphone-et-android-1566932.html">installer le site comme une application mobile (PWA)</a>
+2. Ouvrez les paramètres pour customiser votre interface ( DarkMode / LightMode ).
+3. Vous pouvez ajouter des cryptomonnaies en "Favoris" depuis les paramètres, les favoris apparaîtront en premier dans votre barre de navigation.
+4. Pour chercher une cryptomonnaie, tapez son nom dans la barre de recherche.
+5. Vous pouvez suivre <a href="https://twitter.com/Cosmosdocs">@CosmosDocs</a> sur les réseaux.
+6. CosmosDocs et son équipe sont indépendants, nous ne vous inciterons donc jamais à l'achat et la vente d'une cryptomonnaie.
+7. Pour toute question, l'équipe est disponible via Twitter ou par email à <a HREF="mailto:cosmosdocs@protonmail.com">cosmosdocs@protonmail.com</a>.
--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,15 @@
 <template>
-  <div class="flex justify-center">
-    <div class="flex flex-col items-center justify-center text-justify px-6 space-y-4 lg:w-5/6 xl:w-4/6">
-        <h1 class="text-3xl text-center">Bienvenue sur CosmosDocs.io !</h1>
-        <div class="space-y-4">
-          <p><strong>CosmosDocs</strong> est un site proposant de la documentation, de la vulgarisation et de l'actualité sur l'écosystème Cosmos (ATOM). Le site est en ligne depuis le mois d'Août 2022 et compte à ce jour 6 collaborateurs / développeurs / rédacteurs.</p>
-          <p>Nous essayons sur ce site de proposer du contenu simple, accessible et surtout gratuit ! Dans l'objectif d'étendre le périmètre de l'écosystème Cosmos à la communauté francophone.</p>
+  <div class="flex flex-col items-center space-y-6">
+    <div class="flex flex-row items-center justify-center space-x-4 mt-4">
+        <div class="flex">
+            <img width="100" src="/icon.png" alt="Le logo de CosmosDocs" />       
         </div>
-        <div class="space-y-4">
-          <p>Pour chaque blockchain, vous retrouverez:</p>
-          <ul class="space-y-4 ml-8 list-disc">
-            <li>Un canal <strong>Actualité</strong>: Relayant les infos et news les plus croustillantes du moment.</li>
-            <li>Un canal <strong>Gouvernance</strong>: Sur lequel nous essayons de traduire et vulgariser les votes de gouvernances des cryptomonnaies.</li>
-            <li>Un canal <strong>Airdrop</strong>: Qui ne sera pas forcèment présent sur toutes les cryptomonnaies, car les airdrops sont généralement donnés à la mise en place du Token. Mais qui pourra contenir les airdrops pour lequel posséder et/ou staker le token peut amener à une éligibilité.</li>
-            <li>Un canal <strong>Staking</strong>: Comprenant les news et données relatives au staking (Validateurs, Rendement, APY, etc).</li>
-          </ul>
-        </div>
-        <div class="space-y-4">
-          <p>Voici ce que vous devez savoir avant de démarrer :</p>
-          <ol class="list-decimal ml-8 space-y-4">
-            <li>Vous pouvez <a href="https://www.01net.com/astuces/comment-installer-une-version-allegee-de-vos-programmes-preferes-sur-iphone-et-android-1566932.html">installer le site comme une application mobile (PWA)</a></li>
-            <li>Ouvrez les paramètres pour customiser votre interface ( DarkMode / LightMode ).</li>
-            <li>Vous pouvez ajouter des cryptomonnaies en "Favoris" depuis les paramètres, les favoris apparaîtront en premier dans votre barre de navigation.</li>
-            <li>Pour chercher une cryptomonnaie, tapez son nom dans la barre de recherche.</li>
-            <li>Vous pouvez suivre <a href="https://twitter.com/Cosmosdocs">@CosmosDocs</a> sur les réseaux.</li>
-            <li>CosmosDocs et son équipe sont indépendants, nous ne vous inciterons donc jamais à l'achat et la vente d'une cryptomonnaie.</li>
-            <li>Pour toute question, l'équipe est disponible via Twitter ou par email à <a HREF="mailto:cosmosdocs@protonmail.com">cosmosdocs@protonmail.com</a>.</li>
-          </ol>
-      </div>
-      <p>Si tu aimes le concept, notre contenu et que tu souhaites soutenir notre travail et le voir évoluer :</p>
-      <button class="mx-auto bg-green-400 rounded py-3 px-3 text-white font-bold">BUY ME A COFFEE !</button>
+        <h1 class="text-4xl xl:text-6xl">CosmosDocs.io</h1>
+    </div>
+    <div class="mx-auto w-5/6 xl:w-4/6 text-justify leading-relaxed flex flex-col justify-center items-center ">
+        <nuxt-content class="space-y-4" :document="home"/>
+        <p class="mt-4">Si tu aimes le concept, notre contenu et que tu souhaites soutenir notre travail et le voir évoluer :</p>
+        <button class="bg-green-400 rounded py-3 px-3 text-white font-bold w-60 mt-4">BUY ME A COFFEE !</button>
     </div>
   </div>
 </template>
@@ -45,13 +26,21 @@ export default {
         },
       ]
     }
-  }
+  },
+  async asyncData({ $content, params, store}) {
+        const home = await $content("home").fetch();
+        return { home }
+    },
 }
 </script>
 
 <style scoped>
 a {
   color: #3377c0;
+}
+
+p {
+  text-align: justify;
 }
 
 </style>
